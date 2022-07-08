b73cc83d3a1f78d033be4b616b597a42ca44e675
ronald-macheka <ronymacheka@outlook.com>
2022-07-08 08:06:01 +0200

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -4,31 +4,64 @@ let todoInput = document.getElementById('todo-input');
 const addTodoBtn = document.getElementById('add-todo-btn');
 //get fererence to the hero section to display to do items
 const heroSectionElement = document.getElementById('hero-section');
-//body
-let body = document.get
+//get reference to edit button
 
 //add button event handler
 function addTodo() {
-    //prevent browser from loading
-    event.preventDefault();
-    //get user input and store in a variable
-    let input = todoInput.value;
-    //clear todo input field
-    todoInput.value = '';
-    //test if function works by printing to the console
-    console.log(input)
-    //create div element
-    let div = document.createElement('div');
-    //add content to the div element
-    div.textContent = input;
-    //append div to hero section
-    heroSectionElement.append(div);
-    
-}
+
+    if (todoInput.value === '') {
+        alert('Please enter valid todo task!')
+    } else {
+        //prevent form from submitting
+        event.preventDefault();
+        //get user input and store in a variable
+        let input = todoInput.value;
+        //clear todo input field
+        todoInput.value = '';
+        //test if function works by printing to the console
+        console.log(input)
+        //create input element to output todo task
+        let taskDisplayElement = document.createElement('input');
+        //add content to the task output element
+        taskDisplayElement.value = input;
+        taskDisplayElement.setAttribute('readonly', 'readonly')
+    //append task output to hero section
+    heroSectionElement.append(taskDisplayElement);
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'edit'
+    heroSectionElement.append(editBtn);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'delete';
+    heroSectionElement.append(deleteBtn);
+  
+    function deleteTask() {
+        console.log('delete btn cliked')
+        taskDisplayElement.remove()
+        editBtn.remove()
+        deleteBtn.remove()
+        }
+
+        function editTask() {
+            if (editBtn.textContent === 'edit') {
+                taskDisplayElement.removeAttribute('readonly');
+                editBtn.textContent = 'update';
+            } else if (editBtn.textContent === 'update') {
+                taskDisplayElement.setAttribute('readonly', 'readonly')
+                editBtn.textContent = 'edit';
+                return;
+        }
+        
+        }
+
+    editBtn.addEventListener('click', editTask)    
+    deleteBtn.addEventListener('click', deleteTask)
+        
+    }//end of else block
+        
+} //end of addTodo function
 
 //add event listener to Add Todo Button button 
 addTodoBtn.addEventListener('click', addTodo);
-
 
 
 
